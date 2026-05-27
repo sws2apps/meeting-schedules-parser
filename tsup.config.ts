05cbf288d387ec0e59bd27a4a3da715931a0f9bd
rhahao <26148770+rhahao@users.noreply.github.com>
2026-05-27 21:09:42 +0300

    chore: fix dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: ['src/browser/index.ts'],
-    dts: { resolve: true, only: true },
+    dts: { resolve: true, only: true, compilerOptions: { ignoreDeprecations: '6.0' } },
     format: ['esm'], // Ensure ESM output
     clean: true,
     outDir: 'dist',
   },
   {
     entry: ['src/node/index.ts'],
-    dts: { resolve: true, only: true },
+    dts: { resolve: true, only: true, compilerOptions: { ignoreDeprecations: '6.0' } },
     format: ['esm'], // Ensure ESM output
     clean: true,
     outDir: 'dist/node',
